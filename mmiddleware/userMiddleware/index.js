3c981fd23cba143354a6edec4b1fc745e702c9a7
Mateus Davilagit config --global user.email davila_mateus@hotmail.com <davila_mateus@hotmail.com>
2023-10-15 10:10:11 +0100

    v1.1

---
+++ mmiddleware/userMiddleware/index.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const JWTsecret = require('./JWTsecret');
+
+
+
+function auth(req, res) {
+    const authToken = req.headers['authorization'];
+    console.log('teste')
+
+    if (authToken !== undefined) {
+
+        const token = authToken.split(' ')[1];
+        jwt.verify(token, JWTsecret, (error, data) => {
+            if (error) {
+                res.json({ error: 'INVALID TOKEN' });
+            } else {
+                req.user = data;
+                next();
+            }
+        })
+
+    } else {
+        res.json({ error: ' THERE NO TOKEN' });
+    }
+}
+
+
+
+module.exports = auth;
+
