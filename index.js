f20fe413e0e3457432939250992a52d24535a36e
Mateus Davilagit config --global user.email davila_mateus@hotmail.com <davila_mateus@hotmail.com>
2023-10-15 09:01:49 +0100

    v1

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ const modelFinancesEntraces = require('./models/finances/entraces');
 
 
 
-const corsOptions = {
-    origin: 'http://localhost:3001',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+
 app.use(cors())
 
 app.use("/", (req, res) => {
