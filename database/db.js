85c3d66f483f02c2c30edc6905798277f2128a61
Mateus Davilagit config --global user.email davila_mateus@hotmail.com <davila_mateus@hotmail.com>
2023-10-19 17:53:57 +0100

    v1.2

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 
 const connection = new Sequelize('u225794026_lab', 'u225794026_lab', 'Goias123.', {
-    host: '153.92.6.103',
+    host: '62.72.62.70',
     dialect: 'mysql',
 
 });
