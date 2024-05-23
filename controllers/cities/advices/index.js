d0cf43de819d57c0209022e481baa1ac8554f2c6
Mateus D'avila <davila_mateus@hotmail.com>
2024-05-23 11:22:25 +0100

    23 de maio 2024

--- controllers/cities/advices/index.js
+++ controllers/cities/advices/index.js
@@ -1,38 +1,44 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const axios = require('axios');
+const axios = require("axios");
 const apiKey = process.env.foursquare;
 
 const citiesAdvices = async (req, res) => {
     const { lat, lon, category } = req.query;
     if (lat && lon && category) {
         const options = {
-            method: 'GET',
+            method: "GET",
             headers: {
-                accept: 'application/json',
+                accept: "application/json",
                 Authorization: ` ${apiKey}`
             }
         };
 
-        const places = await axios.get(`https://api.foursquare.com/v3/places/search?local=${lat},${lon}${category ? `&categories=${category}` : ''}`, options)
+        const places = await axios
+            .get(`https://api.foursquare.com/v3/places/search?local=${lat},${lon}${category ? `&categories=${category}` : ""}`, options)
             .then((data) => {
-                return data.data.results
+                return data.data.results;
             })
             .catch((error) => {
+                console.log("adivices", error);
                 res.json(error).status(400);
+            });
+
+        const placesWithImages = await Promise.all(
+            places.map(async (place) => {
+                return (imageResponse = await axios
+                    .get(`https://api.foursquare.com/v3/places/${place.fsq_id}/photos`, options)
+                    .then((images) => {
+                        return { ...place, images: images.data };
+                    })
+                    .catch((error) => {
+                        console.log("Imagens", error);
+
+                        res.json(error).status(400);
+                    }));
             })
-
-
-        const placesWithImages = await Promise.all(places.map(async (place) => {
-            return imageResponse = await axios.get(`https://api.foursquare.com/v3/places/${place.fsq_id}/photos`, options)
-                .then((images) => {
-                    return { ...place, images: images.data };
-                })
-                .catch((error) => {
-                    res.json(error).status(400);
-                })
-        }))
-        res.status(200).json(placesWithImages)
+        );
+        res.status(200).json(placesWithImages);
     }
 };
 
