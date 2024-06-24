66b9a3bdafd3ccb6312a267cbf3e0230cb98c277
Mateus D'avila <davila_mateus@hotmail.com>
2024-06-24 19:31:17 +0100

    try

--- database/db.js
+++ database/db.js
@@ -1,12 +1,10 @@
-const Sequelize = require('sequelize');
+require("dotenv").config();
+const Sequelize = require("sequelize");
+const apiKey = process.env.db;
 
-
-const connection = new Sequelize('u225794026_lab', 'u225794026_lab', 'Goias123.', {
-    host: '62.72.62.70',
-    dialect: 'mysql',
-
+const connection = new Sequelize("u225794026_lab", "u225794026_lab", apiKey, {
+    host: "62.72.62.70",
+    dialect: "mysql"
 });
 
 module.exports = connection;
-
-
